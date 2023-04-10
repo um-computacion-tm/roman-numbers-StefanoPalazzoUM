75e49a2f25e38479eded2461921d2c690c245126
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-10 18:28:56 -0300

    Decimales a romanos hasta el 10

---
+++ decimal_to_roman.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def decimal_to_roman(decimal):
+    if decimal <= 3:
+        return 'I' * decimal
+    elif decimal == 5:
+        return 'V'
+    else:
+        return "X"
+
+if __name__ == '__main__':
+    unittest.main()
